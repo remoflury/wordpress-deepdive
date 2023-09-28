b82ee9ab48327a386714b51db0a99482e4901712
remoflury <remoflury97+web@gmail.com>
2023-09-28 13:53:02 +0200

    spacing titles

--- src/routes/[...slug]/+page.svelte
+++ src/routes/[...slug]/+page.svelte
@@ -4,5 +4,7 @@
 	console.log(data.page);
 </script>
 
-<h1>{data.page.title.rendered}</h1>
-<p>{@html data.page.content.rendered}</p>
+<section class="container">
+	<h1>{data.page.title.rendered}</h1>
+	<p>{@html data.page.content.rendered}</p>
+</section>
