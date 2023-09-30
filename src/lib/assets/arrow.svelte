ee86d66c27ecbf05182b18e01fdcbd46ced36b04
remoflury <remoflury97+web@gmail.com>
2023-09-30 13:00:25 +0200

    add arrows to slider

---
+++ src/lib/assets/arrow.svelte
@@ -0,0 +1,21 @@
+<svg
+	class="max-w-full"
+	version="1.1"
+	id="Ebene_1"
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	x="0px"
+	y="0px"
+	viewBox="0 0 8.8 14.6"
+	style="enable-background:new 0 0 8.8 14.6;"
+	xml:space="preserve"
+>
+	<!-- <style type="text/css">
+	.st0{fill:#bbd1ea;}
+</style> -->
+	<path
+		id="Pfad_1834"
+		class="fill-offwhite"
+		d="M1.6,14.6L0,13l5.7-5.7L0,1.6L1.6,0l7.3,7.3L1.6,14.6z"
+	/>
+</svg>
--- src/lib/components/slider.svelte
+++ src/lib/components/slider.svelte
@@ -3,6 +3,7 @@
 	import { Splide, SplideSlide, SplideTrack } from '@splidejs/svelte-splide';
 	// or only core styles
 	import '@splidejs/svelte-splide/css/core';
+	import Arrow from '$lib/assets/arrow.svelte';
 
 	export let content: SlideshowProps;
 
@@ -22,27 +23,44 @@
 		lazyload: true,
 		pauseOnHover: true,
 		interval: 4000,
-		arrows: false
+		arrows: true,
+		pagination: false
 	};
 </script>
 
-<article class="bg-blue-dark">
+<article class="bg-blue-dark pb-8">
 	<div class="container content-grid">
 		<h2 class="text-offwhite lg:col-span-6 lg:mb-12">{content.titleH2}</h2>
 		<div class="lg:col-span-8 lg:col-start-3">
-			<Splide {options}>
-				{#each slides as slide, index (index)}
-					<SplideSlide>
-						<figure class="aspect-video overflow-hidden">
-							<img
-								class="object-center object-cover w-full h-full"
-								src={slide.url}
-								alt={slide.alt}
-								loading="lazy"
-							/>
-						</figure>
-					</SplideSlide>
-				{/each}
+			<Splide {options} hasTrack={false}>
+				<div class="relative">
+					<div class="splide__arrows absolute flex gap-8 -bottom-4 translate-y-full right-0">
+						<button
+							class="splide__arrow splide__arrow--prev image-slider-btn w-5 aspect-auto rotate-180 hover:opacity-70 transition"
+						>
+							<Arrow />
+						</button>
+						<button
+							class="splide__arrow splide__arrow--next image-slider-btn w-5 aspect-auto hover:opacity-70 transition"
+						>
+							<Arrow />
+						</button>
+					</div>
+					<SplideTrack>
+						{#each slides as slide, index (index)}
+							<SplideSlide>
+								<figure class="aspect-video overflow-hidden">
+									<img
+										class="object-center object-cover w-full h-full"
+										src={slide.url}
+										alt={slide.alt}
+										loading="lazy"
+									/>
+								</figure>
+							</SplideSlide>
+						{/each}
+					</SplideTrack>
+				</div>
 			</Splide>
 		</div>
 	</div>
