1fdfb4a8f0d1918e41902acf67cd3d91919987a9
remoflury <remoflury97+web@gmail.com>
2024-01-15 10:22:43 +0100

    change visitor count to tweened

--- src/lib/components/visitorCounter.svelte
+++ src/lib/components/visitorCounter.svelte
@@ -3,38 +3,26 @@
 	import { onMount } from 'svelte';
 	import WaveWrapper from './ui/waves/waveWrapper.svelte';
 	import viewport from '$lib/utils/viewportObserver';
+	import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
 
 	export let title: string;
-	let counterElem: HTMLParagraphElement;
-	let count: number = 0;
 
 	const getVisitorCount = async () => {
 		const response = await fetch(`${PUBLIC_CMS_URL}/wp-json/snp/v1/number/`);
 		const data = await response.json();
-		return data;
+
+		return Math.floor(parseFloat(data));
 	};
 
-	function animateValue(elem: HTMLElement, start: number, end: number, duration: number) {
-		let startTimestamp: number | null = null;
-		const step = (timestamp: number) => {
-			if (!startTimestamp) startTimestamp = timestamp;
-			const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-			elem.innerHTML = Math.floor(progress * (end - start) + start).toString();
-			if (progress < 1) {
-				window.requestAnimationFrame(step);
-			}
-		};
-		window.requestAnimationFrame(step);
-	}
-
-	onMount(async () => {
-		// count = await getVisitorCount();
-		// animateValue(counterElem, 0, count, count * 40);
+	const count = tweened(0, {
+		duration: 1000,
+		easing: cubicOut
 	});
 
-	$: if (count) {
-		animateValue(counterElem, 0, count, count * 40);
-	}
+	$: roundedCount = Math.round($count);
+
+	// $: console.log($count);
 </script>
 
 <WaveWrapper>
@@ -45,14 +33,13 @@
 			<div class="container" data-aos="fade-up">
 				<p class="font-bold text-2xl lg:text-3xl text-blue-light text-center mb-4">{title}</p>
 				<p
-					bind:this={counterElem}
 					use:viewport
-					on:enterViewport={async () => {
-						count = await getVisitorCount();
-					}}
-					on:exitViewport={() => (count = 0)}
+					on:enterViewport={async () => count.set(await getVisitorCount())}
+					on:exitViewport={() => count.set(0)}
 					class="text-6xl lg:text-8xl text-offwhite font-bold text-center"
-				/>
+				>
+					{roundedCount}
+				</p>
 			</div>
 		{/await}
 	</article>
